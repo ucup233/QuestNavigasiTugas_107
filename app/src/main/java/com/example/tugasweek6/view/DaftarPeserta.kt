b4cf9257289e09b192348b724255f847b06724d8
ucup233 <sufhidayat9@gmail.com>
2025-11-08 20:45:21 +0700

    Pembuatan isipage Daftar Peserta

--- app/src/main/java/com/example/tugasweek6/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/tugasweek6/view/DaftarPeserta.kt
@@ -1,11 +1,90 @@
 package com.example.tugasweek6.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 
+import com.example.tugasweek6.R
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
-    OnSubmitBtnClick()-> Unit,
-    OnBackBtnClick()-> Unit
-){
+    onBackBtnClick: () -> Unit
+) {
+    val items = listOf(
+        "Nama" to "Contoh Nama",
+        "Jenis Kelamin" to "Lainnya",
+        "Asal" to "Yogyakarta"
+    )
 
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Daftar Nama Peserta",
+                        color = Color.White,
+                        fontSize = 40.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.black)
+                )
+            )
+        }
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier
+                .padding(isiRuang)
+                .padding(all = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(
+                space = 1.dp
+            )
+        ) {
+            items.forEach { item ->
+                Column {
+                    Text(
+                        text = item.first,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Cursive,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = item.second,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Cursive,
+                        fontSize = 22.sp
+                    )
+                }
+                HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onBackBtnClick
+            ) {
+                Text(text = "Kembali")
+            }
+        }
+    }
 }
