5079de6e7eef7d5b4608103c25a8dd2841495490
ucup233 <sufhidayat9@gmail.com>
2025-11-08 19:54:31 +0700

    Penambahan logo untuk homepage

--- app/src/main/java/com/example/tugasweek6/view/Home.kt
+++ app/src/main/java/com/example/tugasweek6/view/Home.kt
@@ -1,5 +1,6 @@
 package com.example.tugasweek6.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,13 +9,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.example.tugasweek6.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun Home(
@@ -36,6 +40,8 @@ fun Home(
         )
         Spacer(modifier = Modifier.height(70.dp)
         )
-
+        Image(
+            painter = painterResource(id = R.drawable.logo_umy),
+        )
     }
 }
