cbeec48ffbc893be8bc35bc6b8d841d67a0fedce
ucup233 <sufhidayat9@gmail.com>
2025-11-08 20:45:36 +0700

    Pembuatan navigasi pertama ke homepage

--- app/src/main/java/com/example/tugasweek6/Navigasi.kt
+++ app/src/main/java/com/example/tugasweek6/Navigasi.kt
@@ -1,7 +1,40 @@
 package com.example.tugasweek6
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tugasweek6.view.Home
+
 enum class Navigasi {
     Formulir,
     Detail,
     HomePage
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.HomePage.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+
+            composable(route = Navigasi.HomePage.name) {
+                Home(
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
 }
