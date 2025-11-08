414de7d8975c625bbfe7736630f9432253376b93
ucup233 <sufhidayat9@gmail.com>
2025-11-08 20:24:38 +0700

    Penambahan View DaftarPeserta

---
+++ app/src/main/java/com/example/tugasweek6/view/DaftarPeserta.kt
@@ -0,0 +1,11 @@
+package com.example.tugasweek6.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TampilData(
+    OnSubmitBtnClick()-> Unit,
+    OnBackBtnClick()-> Unit
+){
+
+}
