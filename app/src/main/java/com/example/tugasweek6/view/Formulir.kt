8389b9973ce9cad4e14e19c41cebafc8217d91ac
ucup233 <sufhidayat9@gmail.com>
2025-11-08 20:53:11 +0700

    Pembuatan form Formulir

---
+++ app/src/main/java/com/example/tugasweek6/view/Formulir.kt
@@ -0,0 +1,12 @@
+package com.example.tugasweek6.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Form(modifier: Modifier = Modifier,
+         onBackBtnClick: () -> Unit = {},
+         OnSubmitBtnClick: () -> Unit = {}
+) {
+
+}
