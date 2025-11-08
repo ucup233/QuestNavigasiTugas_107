f98fd018d281c02d4c7b81605438dcfc782412c6
ucup233 <sufhidayat9@gmail.com>
2025-11-08 20:59:46 +0700

    Pembuatan column di dalam box

--- app/src/main/java/com/example/tugasweek6/view/Formulir.kt
+++ app/src/main/java/com/example/tugasweek6/view/Formulir.kt
@@ -1,11 +1,23 @@
 package com.example.tugasweek6.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Form(modifier: Modifier = Modifier,
@@ -24,4 +36,16 @@ fun Form(modifier: Modifier = Modifier,
 
     val gender = listOf("Laki-laki", "Perempuan")
     val status = listOf("Janda", "Lajang", "Duda")
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF5E6F7)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            
+    }
 }
